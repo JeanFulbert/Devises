7726962de86430c7ef8e6660d040d2126d98eafd
Chris <jeanfulbert.horme@mailoo.org>
2016-12-17 19:54:12 +0100

    Add unit test with complex case

--- Devises.Domain.Tests/Currencies.cs
+++ Devises.Domain.Tests/Currencies.cs
@@ -6,5 +6,9 @@
         public static Currency Dollar { get; } = new Currency("USD");
         public static Currency Yuan { get; } = new Currency("CNY");
         public static Currency Yen { get; } = new Currency("JPY");
+        public static Currency SwissFranc { get; } = new Currency("CHF");
+        public static Currency AustralianDollar { get; } = new Currency("AUD");
+        public static Currency Wu { get; } = new Currency("KWU");
+        public static Currency Rupee { get; } = new Currency("INR");
     }
 }
--- Devises.Domain.Tests/GraphSpecs/ShouldReturnShortestComplexPath.cs
+++ Devises.Domain.Tests/GraphSpecs/ShouldReturnShortestComplexPath.cs
@@ -26,5 +26,29 @@
 
             Assert.That(actual, Is.EqualTo(expected));
         }
+
+        [Test]
+        public void WhenRouteIsComplex()
+        {
+            var sut = new ExchangeRateGraph();
+            sut.AddEdge(new ExchangeRateEdge(Currencies.AustralianDollar, Currencies.SwissFranc, new ExchangeRate(0.9661m)));
+            sut.AddEdge(new ExchangeRateEdge(Currencies.Yen, Currencies.Wu, new ExchangeRate(13.1151m)));
+            sut.AddEdge(new ExchangeRateEdge(Currencies.Euro, Currencies.SwissFranc, new ExchangeRate(1.2053m)));
+            sut.AddEdge(new ExchangeRateEdge(Currencies.AustralianDollar, Currencies.Yen, new ExchangeRate(86.0305m)));
+            sut.AddEdge(new ExchangeRateEdge(Currencies.Euro, Currencies.Dollar, new ExchangeRate(1.2989m)));
+            sut.AddEdge(new ExchangeRateEdge(Currencies.Yen, Currencies.Rupee, new ExchangeRate(0.6571m)));
+
+            var actual = sut.GetShortestPathBetween(Currencies.Euro, Currencies.Yen);
+
+            var expected =
+                new[]
+                {
+                    new ExchangeRateEdge(Currencies.Euro, Currencies.SwissFranc, new ExchangeRate(1.2053m)),
+                    new ExchangeRateEdge(Currencies.SwissFranc, Currencies.AustralianDollar, new ExchangeRate(1.0351m)),
+                    new ExchangeRateEdge(Currencies.AustralianDollar, Currencies.Yen, new ExchangeRate(86.0305m))
+                };
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
